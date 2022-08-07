7b9f38b58785f806ad5940361aece691ce9ff725
vantage-ola <vantageola4@gmail.com>
2022-08-07 11:59:37 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider, Button } from 'react-native-paper'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Provider>
+        <View style={styles.container}>
+            <Text>Learn Formula 1</Text>
+        <StatusBar style="auto" />
     </View>
+    </Provider>
+
   );
 }
 
+
+{/* formula 1 main color #FF0100*/}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
