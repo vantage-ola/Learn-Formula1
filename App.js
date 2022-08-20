64288687c8ee8c0d9f72c17295068faffa4a77d9
vantage-ola <vantageola4@gmail.com>
2022-08-20 18:16:16 +0100

    changed theme

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Provider, Button, Title, Paragraph } from 'react-native-paper';
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
 import HomeScreen from './screens/home';
 
 export default function App() {
   return (
+    <PaperProvider theme={theme}>
         <HomeScreen/>
+    </PaperProvider>
   );
 }
 
 
 {/* formula 1 main color #FF0100*/}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    version: 3,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#ff0100',
+      secondary: '#000000',
+      tertiary: '#c9c9c9'
+    },
+  };
+  
