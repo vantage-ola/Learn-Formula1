b0364630a945ca46ee25dd44e66794f5ec329e55
vantage-ola <vantageola4@gmail.com>
2022-09-05 21:43:38 +0100

    Update: refactor code

---
+++ screens/team.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { BASE_API_URL } from "../config";
+import {TabScreen} from 'react-native-paper-tabs';
+import { SafeAreaView, FlatList, View } from "react-native";
+import { Card, ActivityIndicator, IconButton } from "react-native-paper";
+
+function TeamScreen() {
+
+    const [teams, setTeams] = useState([])
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(`${BASE_API_URL}2022/constructors.json`)
+        .then((response) => response.json())
+        .then((json) => setTeams(json.MRData.ConstructorTable))
+        .finally(() => setLoading(false))
+    }
+    , []);
+    return (
+            <View style={{  flex:1 }}>
+            {isLoading ? <ActivityIndicator animating={true} color={'#ff0100'} /> : 
+      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+
+          <FlatList
+            data={teams.Constructors}
+            keyExtractor={({ constructorId }, index) => constructorId}
+            renderItem={({ item }) => (
+                <Card mode="outlined" onPress={()=> {}}>
+                    <Card.Content>
+                        <Card.Title
+                        title={item.name}
+                        subtitle={`${item.nationality} Team`}
+                        subtitleStyle={{fontSize: 15}}
+                        right={(props) => <IconButton {...props} icon="chevron-right" />}
+                        />
+                        
+                    </Card.Content>
+                </Card>
+            )}
+          />
+        </View>
+      )}
+            </View>
+
+    )
+}
+
+export default TeamScreen;
