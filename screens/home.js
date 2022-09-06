a7b24b680cefb6613b88e4112d6d835b7c4577e1
vantage-ola <vantageola4@gmail.com>
2022-09-06 14:47:27 +0100

    fix: refactor code

--- screens/home.js
+++ screens/home.js
@@ -1,30 +1,20 @@
 import React, {useState} from "react";
-import { View, Platform} from 'react-native';
+import { View} from 'react-native';
 
-import { Button } from "react-native-paper";
 import {Tabs, TabScreen} from 'react-native-paper-tabs';
-import { Appbar } from 'react-native-paper';
 
+import Header from "./header";
 import TeamScreen from "./team";
 import CircuitScreen from "./circuit";
 import DriverScreen from "./driver";
 
 function HomeScreen() {
-    
-    const [year, setYear ] = useState(2022) // year season
-    const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical'; // change icon based on platform
 
     return (
 
     <View style={{flex: 1}}>
 
-        {/* Header, contains title, edit and side bar button */}
-        <Appbar.Header>
-            <Appbar.Action icon={MORE_ICON} onPress={() => {}} />            
-            <Appbar.Content titleStyle={{fontWeight: "bold", fontSize: 25 }} title={`${year} Season`} />
-            <Button color="white" >Edit</Button>
-        </Appbar.Header>
-
+        <Header/>
       {/* Tabs, contains swipeable rendered data*/}  
       <Tabs>
         
@@ -39,7 +29,7 @@ function HomeScreen() {
         <TabScreen label="Circuits" >
             <CircuitScreen/>
         </TabScreen>
-        
+
       </Tabs>
     </View>
     )
