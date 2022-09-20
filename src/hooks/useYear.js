5b08066f5a6eb2edded6e9e83df0f922c2fa3aaf
vantage-ola <vantageola4@gmail.com>
2022-09-20 17:05:27 +0100

    + local stored state

--- src/hooks/useYear.js
+++ src/hooks/useYear.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import { useHookstate } from '@hookstate/core';
+import { localstored } from '@hookstate/localstored';
 
 export const useYear = () =>  {
 
     const presentYear = new Date().getFullYear().toString()
-    const year = useHookstate(presentYear);
+    const year = useHookstate(presentYear, localstored({
+        // key is optional,
+        // if it is not defined, the extension requires and
+        // uses the identifier from the @hookstate/identifiable
+        key: 'state-key'
+    }));
 
     const wrapState = (y) => ({
     get: () => y.value,
